6debb87a69708dfc12105b0d28530d55de2eee96
unknown <bulatsrc@gmail.com>
2022-11-14 12:24:00 +0300

    refactoring

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -21,40 +21,40 @@ const Users = () => {
         return <span className="badge bg-danger">Никто с тобой не тусанет</span>;
     };
 
+    const renderTitles = () => {
+        return tableTitles.map((title, i) => (
+            <th key={title} colSpan={tableTitles.length === i + 1 ? 2 : 1}>{title}</th>
+        ))
+    }
+
+    const renderUsers = () => {
+       return users.map(({ _id, name, qualities, profession: { name: professionName }, completedMeetings, rate }) => (
+            <tr key={_id}>
+                <th>{name}</th>
+                <td>
+                    {qualities.map(({ _id, name, color }) => (
+                        <span className={`badge me-1 bg-${color}`} key={_id}>{name}</span>
+                    ))}
+                </td>
+                <td>{professionName}</td>
+                <td>{completedMeetings}</td>
+                <td>{rate}</td>
+                <td>
+                    <button className="btn btn-danger" onClick={() => { handleDelete(_id); }}>Delete</button>
+                </td>
+            </tr>
+        ))
+    }
+
     return (
         <>
             <h2>{renderPhrase()}</h2>
             {users.length > 0 && (
                 <table className="table">
                     <thead>
-                        <tr>
-                            {tableTitles.map((title, i) => (
-                                tableTitles.length === i + 1
-                                    ?
-                                    <th key={title} colSpan={2}>{title}</th>
-                                    :
-                                    <th key={title}>{title}</th>
-                            ))}
-                        </tr>
+                        <tr>{renderTitles()}</tr>
                     </thead>
-                    <tbody>
-                        {users.map(({ _id, name, qualities, profession: { name: professionName }, completedMeetings, rate }) => (
-                            <tr key={_id}>
-                                <th>{name}</th>
-                                <td>
-                                    {qualities.map(({ _id, name, color }) => (
-                                        <span className={`badge me-1 bg-${color}`} key={_id}>{name}</span>
-                                    ))}
-                                </td>
-                                <td>{professionName}</td>
-                                <td>{completedMeetings}</td>
-                                <td>{rate}</td>
-                                <td>
-                                    <button className="btn btn-danger" onClick={() => { handleDelete(_id); }}>Delete</button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
+                    <tbody>{renderUsers()}</tbody>
                 </table>
             )}
         </>
